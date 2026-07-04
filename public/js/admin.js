925e2aadc5c5b2f18eab2786bc5fe622f4e8ef20
Ordi Zouzou <galouche.rustler@gmail.com>
2026-07-04 16:55:36 +0200

    Escape quote characters in escapeHtml to close attribute-context XSS gap

--- public/js/admin.js
+++ public/js/admin.js
@@ -171,7 +171,7 @@
   function escapeHtml(str) {
     const div = document.createElement('div');
     div.textContent = str;
-    return div.innerHTML;
+    return div.innerHTML.replace(/"/g, '&quot;').replace(/'/g, '&#39;');
   }
 
   async function loadDashboard() {
